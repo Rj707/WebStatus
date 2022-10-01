0152a58ec22c17919fd7c463e08e6de8579961ee
H.M.Saad <>
2022-10-01 14:08:50 +0500

    initial commit.

---
+++ WebStatus/WebStatus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebStatus
+//
+//  Created by Hafiz Muhammad Saad on 01/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
